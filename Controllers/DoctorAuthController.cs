d73af70662bfb36bd13e3f4e78157499bd47f338
Abraar-kanini <abraar.kanini@gmail.com>
2024-02-11 00:46:06 +0530

    Added the repository and the service class

--- Controllers/DoctorAuthController.cs
+++ Controllers/DoctorAuthController.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography;
 using DoctorFinderHubApi.Data;
 using DoctorFinderHubApi.Dto.DoctorAuth;
 using DoctorFinderHubApi.Models;
+using DoctorFinderHubApi.Services.DoctorServices.Interfaces;
 using MailKit.Security;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,11 +20,13 @@ namespace DoctorFinderHubApi.Controllers
     public class DoctorAuthController : ControllerBase
     {
         private readonly DoctorFinderHubApiDbContext doctorFinderHubApiDbContext;
+        private readonly IDoctorService doctorService;
 
-        public DoctorAuthController(DoctorFinderHubApiDbContext doctorFinderHubApiDbContext,IConfiguration configuration)
+        public DoctorAuthController(DoctorFinderHubApiDbContext doctorFinderHubApiDbContext,IConfiguration configuration, IDoctorService doctorService)
         {
             this.doctorFinderHubApiDbContext = doctorFinderHubApiDbContext;
             Configuration = configuration;
+            this.doctorService = doctorService;
         }
 
         public IConfiguration Configuration { get; }
@@ -36,8 +39,8 @@ namespace DoctorFinderHubApi.Controllers
             {
                 return BadRequest("Doctor Already Exist");
             }
-
-            CreatePasswordHash(DoctorAuthPostDto.Password, out byte[] PasswordHash, out byte[] PasswordSalt);
+            doctorService.CreatePasswordHash(DoctorAuthPostDto.Password, out byte[] PasswordHash, out byte[] PasswordSalt);
+           
             var doctordeatils = new DoctorAuth
             {
                 DoctorName = DoctorAuthPostDto.DoctorName,
@@ -48,72 +51,12 @@ namespace DoctorFinderHubApi.Controllers
                 PasswordHash = PasswordHash,
                 PasswordSalt = PasswordSalt
             };
-            doctordeatils.VerificationToken = CreateToken(doctordeatils);
+            doctordeatils.VerificationToken = doctorService.CreateToken(doctordeatils);
 
-            SendMail(doctordeatils.VerificationToken, doctordeatils.Email);
-            doctorFinderHubApiDbContext.doctorAuths.Add(doctordeatils);
-            await doctorFinderHubApiDbContext.SaveChangesAsync();
+            doctorService.SendMail(doctordeatils.VerificationToken, doctordeatils.Email);
+           
+           await doctorService.AddDoctorAsync(doctordeatils);
             return Ok("User successfully created!");
-
-        }
-
-
-        private void CreatePasswordHash(string Password,out byte[] PasswordHash,out byte[] PasswordSalt) {
-        
-        using (var hmac= new HMACSHA512())
-            {
-                PasswordSalt = hmac.Key;
-                PasswordHash=hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Password)); 
-
-            }
-        
-        }
-
-
-        private string CreateToken(DoctorAuth doctorAuth)
-        {
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, doctorAuth.DoctorName),
-                new Claim(ClaimTypes.Role, "Admin")
-            };
-
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
-                Configuration.GetSection("AppSettings:Token").Value));
-
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds);
-
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
-            return jwt;
-        }
-
-
-        private void SendMail(string token, string recipientEmail)
-        {
-
-
-            var emailMessage = new MimeMessage();
-
-            emailMessage.From.Add(MailboxAddress.Parse("jabraar01@gmail.com"));
-            emailMessage.To.Add(MailboxAddress.Parse(recipientEmail));
-            emailMessage.Subject = "Registered Successfully";
-
-            // Concatenate the random number with the email body
-            string body = $"Your token  is: {token} ";
-            emailMessage.Body = new TextPart(TextFormat.Html) { Text = body };
-
-            using var smtp = new MailKit.Net.Smtp.SmtpClient();
-            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate("jabraar01@gmail.com", "vcfg espi csts buzv");
-            smtp.Send(emailMessage);
-            smtp.Disconnect(true);
-
 
         }
 
---
+++ Repository/DoctorRepo/Implementations/DoctorRepo.cs
@@ -0,0 +1,21 @@
+﻿using DoctorFinderHubApi.Data;
+using DoctorFinderHubApi.Models;
+using DoctorFinderHubApi.Repository.DoctorRepo.Interfaces;
+
+namespace DoctorFinderHubApi.Repository.DoctorRepo.Implementations
+{
+    public class DoctorRepo : IDoctorRepo
+    {
+        private readonly DoctorFinderHubApiDbContext doctorFinderHubApiDbContext;
+
+        public DoctorRepo(DoctorFinderHubApiDbContext doctorFinderHubApiDbContext)
+        {
+            this.doctorFinderHubApiDbContext = doctorFinderHubApiDbContext;
+        }
+        public async Task AddDoctorAsyncrepo(DoctorAuth doctorAuth)
+        {
+            doctorFinderHubApiDbContext.doctorAuths.Add(doctorAuth);
+            await doctorFinderHubApiDbContext.SaveChangesAsync();
+        }
+    }
+}
