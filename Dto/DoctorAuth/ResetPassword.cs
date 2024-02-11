caea7922e9cfec58a6b3a7a44a7cdde4755aef7e
Abraar-kanini <abraar.kanini@gmail.com>
2024-02-11 13:34:05 +0530

    Added the Forget Password And Email Verification

---
+++ Dto/DoctorAuth/ResetPassword.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DoctorFinderHubApi.Dto.DoctorAuth
+{
+    public class ResetPassword
+    {
+        [Required]
+        public string Token { get; set; } = string.Empty;
+        [Required, MinLength(6, ErrorMessage = "Please enter at least 6 characters, dude!")]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
