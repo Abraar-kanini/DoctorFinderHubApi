4618f6c5f344e5225d0de055f256bb6f33a1408b
Abraar-kanini <abraar.kanini@gmail.com>
2024-02-14 21:49:53 +0530

    Added New Doctor Controller For Image Posting

---
+++ Dto/Doctor/DoctorPostDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DoctorFinderHubApi.Dto.Doctor
+{
+    public class DoctorPostDto
+    {
+        public IFormFile File { get; set; }
+
+        public string fileName { get; set; }
+
+        public string? FileDescription { get; set; }
+        public Guid DoctorAuthId { get; set; }
+
+    }
+}
